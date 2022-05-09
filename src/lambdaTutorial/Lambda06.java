a2bf719ae377886a3bc8ceb92e0cf99e3e0637c0
M.Arif Özfidan <ozfidanarif@gmail.com>
2022-05-09 18:58:38 +0200

    09.05.2022

--- src/lambdaTutorial/Lambda06.java
+++ src/lambdaTutorial/Lambda06.java
@@ -7,7 +7,9 @@ import java.util.Arrays;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 public class Lambda06 {
+
     public static void main(String[] args) throws IOException {
+
         //TASK 01 --> haluk.txt dosyasini okuyunuz.(Console'a print ediniz.)
         System.out.println("\nTASK 01 --> haluk.txt dosyasini okuyunuz -->  ");
       //  Path haluk = Path.of("src/lambdaTutorial/haluk.txt");//path haluk obj atandi
@@ -18,11 +20,16 @@ public class Lambda06 {
                         forEach(System.out::println);//akısdaki datalar(her satırdaki string) print edildi.
         //2.yol..
         Files.lines(Paths.get("src/lambdaTutorial/haluk.txt")).forEach(System.out::println);
+
+
         //TASK 02 --> haluk.txt dosyasini buyuk harflerle okuyunuz.(Console'a buyuk harflerle print ediniz.)
         System.out.println("\nTASK 02 --> haluk.txt dosyasini buyuk harflerle okuyunuz -->  ");
         Files.lines(Paths.get("src/lambdaTutorial/haluk.txt")).
                 map(String::toUpperCase).//akısdaki datalar buyuk harfe update edldi
                 forEach(System.out::println);//akısadaki datalar(her satırdaki string)yazıldı)
+
+
+
         //TASK 03 --> haluk.txt dosyasindaki ilk satiri kucuk harflerle print ediniz..
         System.out.println("\nTASK 03 --> haluk.txt dosyasindaki ilk satiri kucuk harflerle okuyunuz  -->  ");
         //1. yol limit();
@@ -34,12 +41,17 @@ public class Lambda06 {
         System.out.println( Files.lines(Paths.get("src/lambdaTutorial/haluk.txt")).
                 map(String::toLowerCase).
                 findFirst());
+
+
         //TASK 04 --> haluk.txt dosyasinda "basari" kelimesinin kac satirda gectiginiz print ediniz.
         System.out.println("\nTASK 04 --> haluk.txt dosyasinda basari kelimesinin kac satirda gectiginiz yazdiriniz -->  ");
         System.out.println( Files.lines(Paths.get("src/lambdaTutorial/haluk.txt")).
                 map(String::toLowerCase).
                 filter(t -> t.contains("basari")).
                 count());
+
+
+
         //TASK 05 --> haluk.txt dosyasindaki farkli kelimeleri  print ediniz..
         System.out.println("\nTASK 05 --> haluk.txt dosyasindaki farkli kelimeleri  yazdiriniz. -->  ");
 /*
@@ -62,6 +74,9 @@ public class Lambda06 {
                 map(t -> t.split(" ")).//satırlardaki kelimeler arraya atandı
                         flatMap(Arrays::stream).
                 collect(Collectors.toSet()));
+
+
+
         //TASK 06 --> haluk.txt dosyasindaki tum kelimeleri natural order  print ediniz..
         System.out.println("\nTASK 06 --> haluk.txt dosyasindaki tum kelimeleri natural order  yazdiriniz. -->  ");
         Files.lines(Paths.get("src/lambdaTutorial/haluk.txt")).
@@ -69,6 +84,8 @@ public class Lambda06 {
                 flatMap(Arrays::stream).//2D arraydaki elelmanlar tek eleman olarak akısa alındı
                 sorted().//harf sırası yapıldı
                 forEach(System.out::println);//print edildi
+
+
         //TASK 07 --> haluk.txt dosyasinda "basari" kelimesinin kac kere gectigini buyuk harf kucuk harf bagımsız print ediniz.
         System.out.println("\nTASK 07 --> haluk.txt dosyasinda basari kelimesinin kac kere gectigini  yazdiriniz. -->  ");
         System.out.println( Files.lines(Paths.get("src/lambdaTutorial/haluk.txt")).
@@ -76,6 +93,8 @@ public class Lambda06 {
                 flatMap(Arrays::stream).
                 filter(t -> t.equals("basari")).
                 count());
+
+
         //TASK 08 --> haluk.txt dosyasinda "a" harfi gecen kelimelerin sayisini print eden  programi create ediniz.
         System.out.println("\nTASK 08 --> haluk.txt dosyasinda a harfi gecen kelimelerin sayisini ekrana yazdiran programi yazdiriniz. -->  ");
         System.out.println( Files.lines(Paths.get("src/lambdaTutorial/haluk.txt")).//txt dosyasıne erişldi
@@ -83,6 +102,8 @@ public class Lambda06 {
                 flatMap(Arrays::stream).//her satırdaki her kelime akısa alındı
                 filter(t -> t.contains("a")).//a bulunduran kelimeler fitrelenedi
                 count());//a bulunduran kelimeler sayıldı
+
+
         //TASK 09 --> haluk.txt dosyasinda icinde "a" harfi gecen kelimeleri print ediniz.
         System.out.println("\nTASK 09 --> haluk.txt dosyasinda a harfi gecen kelimeler yazdiriniz. -->  ");
         System.out.println( Files.lines(Paths.get("src/lambdaTutorial/haluk.txt")).
@@ -90,6 +111,8 @@ public class Lambda06 {
                         flatMap(Arrays::stream).//her satırdaki her kelime akısa alındı
                         filter(t -> t.contains("a")).//a bulunduran kelimeler fitrelenedi
                         collect(Collectors.toList()));
+
+
         //TASK 10 --> haluk.txt dosyasinda kac /farklı harf kullanildigini print ediniz.
         System.out.println("\nTASK 10 --> haluk.txt dosyasinda kac /farklı harf kullanildigini  yazdiriniz. -->  ");
         System.out.println( Files.lines(Paths.get("src/lambdaTutorial/haluk.txt")).
@@ -99,6 +122,9 @@ public class Lambda06 {
                         flatMap(Arrays::stream).
                 distinct().
                 count());
+
+
+
         //TASK 11 --> haluk.txt dosyasinda kac farkli kelime kullanildigini print ediniz.
         System.out.println("\nTASK 11 --> haluk.txt dosyasinda kac farkli kelime kullanildigini  yazdiriniz. -->  ");
         System.out.println( Files.lines(Paths.get("src/lambdaTutorial/haluk.txt")).
@@ -107,6 +133,9 @@ public class Lambda06 {
                         flatMap(Arrays::stream).
                 distinct().
                 count());
+
+
+
         //TASK 12 --> haluk.txt dosyasinda  farkli kelimeleri print ediniz
         System.out.println("\nTASK 12 --> haluk.txt dosyasinda kac farkli kelime kullanildigini  yazdiriniz. -->  ");
         Files.lines(Paths.get("src/lambdaTutorial/haluk.txt")).
